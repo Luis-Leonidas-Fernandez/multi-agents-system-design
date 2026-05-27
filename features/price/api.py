4d0b286bb00a3a9e71607e57749dca70d0c47ffa
luis <fernandezluis303@gmail.com>
2026-05-26 22:24:06 -0300

    fix(price): restore stable feature barrel

--- features/price/api.py
+++ features/price/api.py
@@ -1,19 +1,4 @@
 """Stable public API for the price feature slice."""
 
-from features.price.application.api import (
-    CRYPTO_KEYWORDS,
-    _COIN_ALIASES,
-    _COIN_TICKER,
-    _get_crypto_price_fn,
-    extract_price_from_text,
-    get_crypto_price,
-)
-
-__all__ = [
-    "_COIN_ALIASES",
-    "_COIN_TICKER",
-    "_get_crypto_price_fn",
-    "CRYPTO_KEYWORDS",
-    "get_crypto_price",
-    "extract_price_from_text",
-]
+from features.price.application.api import *  # noqa: F401,F403
+from features.price.application.api import __all__ as __all__
